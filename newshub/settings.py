250c39922b0727b017ea92825b4f6f8879db64bb
Oksana Kotlyarenko <oksanka.kot@gmail.com>
2024-03-17 18:34:48 +0200

    Staticfiles managing

--- newshub/settings.py
+++ newshub/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
+import dj_database_url
 from pathlib import Path
 import cloudinary_storage
 
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-yc_$b-!&yp865u($7hsy1)7@p(se2)+avt(leaik#rfeqqa56="
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "django-insecure-yc_$b-!&yp865u($7hsy1)7@p(se2)+avt(leaik#rfeqqa56=")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1"]
 
 
 # Application definition
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -84,6 +86,11 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES["default"].update(db_from_env)
+
+# DATABASE_URL = "postgres://jaqyrugb:YRDs53jlRdnqJ1WP1Kud4BTYuNkp9C7x@balarama.db.elephantsql.com/jaqyrugb"
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -120,7 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = "staticfiles/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "newshub/static"),
 ]
@@ -137,3 +144,5 @@ CLOUDINARY_STORAGE = {
 }
 
 DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
+LOGIN_REDIRECT_URL = "/"
